c69b693250bd717f882b8cc1e0816e5e543a17de
陽菜 金指 <k023g0099@g.neec.ac.jp>
2024-04-15 14:29:19 +0900

    a

--- main.cpp
+++ main.cpp
@@ -22,6 +22,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	win = WinApp::GetInstance();
 	win->CreateGameWindow(L"LD2B_03_カナサシ_ハルナ_AL3");
 
+
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
 	dxCommon->Initialize(win);
